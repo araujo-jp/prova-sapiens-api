beadfcc3ef4c4bd1df38494394b2df5c5a7d1b85
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-02 16:10:23 -0400

    feat: add route list cars

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
 
 const createCarsController = new CreateCarController();
+const listCarsController = new ListCarsController();
 
 const carsRouter = Router();
 
 carsRouter.post('/', createCarsController.handle);
+
+carsRouter.get('/', listCarsController.handle);
 
 export { carsRouter };
