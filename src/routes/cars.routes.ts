4719f94049fae6774ec3461deaea296290daf191
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-02 17:39:57 -0400

    feat: add route to update car

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { DeleteCarController } from '@modules/cars/useCases/deleteCar/DeleteCarController';
 import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
+import { UpdateCarController } from '@modules/cars/useCases/updateCar/UpdateCarController';
 
 const createCarsController = new CreateCarController();
 const listCarsController = new ListCarsController();
 const deleteCarController = new DeleteCarController();
+const updateCarController = new UpdateCarController();
 
 const carsRouter = Router();
 
@@ -14,6 +16,8 @@ carsRouter.post('/', createCarsController.handle);
 
 carsRouter.get('/', listCarsController.handle);
 
-carsRouter.post('/:id', deleteCarController.handle);
+carsRouter.delete('/:id', deleteCarController.handle);
+
+carsRouter.patch('/:id', updateCarController.handle);
 
 export { carsRouter };
