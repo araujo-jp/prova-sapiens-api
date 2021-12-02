61969674a30985f93b1e02c29c618360322e5732
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-02 18:16:20 -0400

    build: config build

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@shared": "./src/shared",
+          "@database": "./src/database",
+          "@routes": "./src/routes",
+          "@modules": "./src/modules",
+        },
+      },
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
