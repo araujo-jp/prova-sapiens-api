a45634d96d66e891d747a8de053595c63a861737
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-02 16:39:30 -0400

    feat: add route to delete car

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { DeleteCarController } from '@modules/cars/useCases/deleteCar/DeleteCarController';
 import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
 
 const createCarsController = new CreateCarController();
 const listCarsController = new ListCarsController();
+const deleteCarController = new DeleteCarController();
 
 const carsRouter = Router();
 
 carsRouter.post('/', createCarsController.handle);
 
 carsRouter.get('/', listCarsController.handle);
+
+carsRouter.post('/:id', deleteCarController.handle);
 
 export { carsRouter };
