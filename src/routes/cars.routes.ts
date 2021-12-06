c05e460d85083899bd5148dfeef7cf1e1a7d74ac
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-06 03:46:42 -0400

    feat: add route show car by id

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -3,18 +3,22 @@ import { Router } from 'express';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { DeleteCarController } from '@modules/cars/useCases/deleteCar/DeleteCarController';
 import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
+import { ShowCarController } from '@modules/cars/useCases/showCar/ShowCarController';
 import { UpdateCarController } from '@modules/cars/useCases/updateCar/UpdateCarController';
 
 const createCarsController = new CreateCarController();
 const listCarsController = new ListCarsController();
 const deleteCarController = new DeleteCarController();
 const updateCarController = new UpdateCarController();
+const showCarController = new ShowCarController();
 
 const carsRouter = Router();
 
 carsRouter.post('/', createCarsController.handle);
 
 carsRouter.get('/', listCarsController.handle);
+
+carsRouter.get('/:id', showCarController.handle);
 
 carsRouter.delete('/:id', deleteCarController.handle);
 
